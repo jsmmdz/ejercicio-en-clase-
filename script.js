343109713407fcbc1555632c5b5e484375cd1630
jsmmdz <jsmejia@unbosque.edu.co>
2026-04-21 00:21:43 -0500

    variables de datos y conexion con el html

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
-// 1. Constantes
+// 1. Constantes//
 const salarioMinimo = 1750905;
 const salarioMinimoIntegral = 22761765;
 const auxilioTransporte = 249095;
 const porcentajePension = 0.04;
 const porcentajeSalud = 0.04;
 const porcentajeFondoSolidaridad = 0.01;
+
+//CONEXIÓN CON EL HTML//
+
+const formDatosGenerales = document.getElementById("datosGenerales");
+
+// 3. VARIABLES PARA LOS DATOS DEL USUARIO
+
+let nombre = "";
+let edad = 0;
+let tipoDocumento = "";
+let numeroDocumento = "";
+let salario = 0;
+let comisiones = 0;
+let totalHorasExtras = 0;
+let nivelRiesgo = "1";
