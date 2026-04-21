20a40393d71e523f00cf77322d57a55b4722f3f9
jsmmdz <jsmejia@unbosque.edu.co>
2026-04-21 00:45:27 -0500

    Se implementa función validar para segmentar a los usuarios por edad y se crea la función calcularNomina para procesar el IBC y las deducciones en base a los datos ingresados en el DOM.

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-// 1. Constantes//
+// Constantes//
 const salarioMinimo = 1750905;
 const salarioMinimoIntegral = 22761765;
 const auxilioTransporte = 249095;
 const porcentajePension = 0.04;
 const porcentajeSalud = 0.04;
 const porcentajeFondoSolidaridad = 0.01;
-
-//CONEXIÓN CON EL HTML//
+const riesgos = [0.00522, 0.01044, 0.02436, 0.04350, 0.06960];
+//CONEXIÓN CON EL DOM//
 
 const formDatosGenerales = document.getElementById("datosGenerales");
 
-// 3. VARIABLES PARA LOS DATOS DEL USUARIO
+// VARIABLES PARA LOS DATOS DEL USUARIO//
 
 let nombre = "";
 let edad = 0;
@@ -20,3 +20,49 @@ let salario = 0;
 let comisiones = 0;
 let totalHorasExtras = 0;
 let nivelRiesgo = "1";
+
+//VALIDACION DE DATOS//
+function validar(edadUsuario) {
+    if (edadUsuario < 18) {
+        stop(); // Requiere que stop() esté definida en otro contexto
+    } else if (edadUsuario >= 18 && edadUsuario < 25) {
+        esUsuarioBenerficiarioPorCotizante();
+    } else if (edadUsuario >= 60) {
+        pension();
+    } else {
+        salarioCalculo(); // Corregido el typo "salaraioCalculo"
+    }
+}
+
+// FUNCIONES UTILITARIAS//
+function calcularPorcentaje(base, porcentaje) {
+    let resultado = base * porcentaje;
+    return resultado;
+}
+
+
+// LÓGICA DE NEGOCIO Y CÁLCULOS//
+
+function calcularNomina() {
+    let ibc = (salario + comisiones + totalHorasExtras) * 0.7;
+    let calculoAuxilioTransporte = salario < (2 * salarioMinimo) ? auxilioTransporte : 0;
+    let calculoSalud = ibc * porcentajeSalud;
+    let calculoFondoSolidaridad = ibc * porcentajeFondoSolidaridad;
+    let calculoPension = ibc >= (4 * salarioMinimo) 
+        ? (ibc * porcentajePension) + calculoFondoSolidaridad 
+        : ibc * porcentajePension;
+    
+    //Se asegura de convertir el nivel de riesgo a entero y restar 1 para coincidir con el índice del array
+    let indiceRiesgo = parseInt(nivelRiesgo) - 1;
+    let calculoArl = indiceRiesgo >= 0 ? ibc * riesgos[indiceRiesgo] : 0;
+
+
+    return {
+        ibc,
+        calculoAuxilioTransporte,
+        calculoSalud,
+        calculoFondoSolidaridad,
+        calculoPension,
+        calculoArl
+    };
+}
